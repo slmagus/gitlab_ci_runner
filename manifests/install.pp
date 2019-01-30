8eaf26b8ef369058633f9296931bab35ea550184
m1zav00 <zach.vida@frb.gov>
2019-01-30 11:48:16 -0500

    corrected the class name

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,10 @@
 # == Class: gitlab_ci_runner::install
 #
-class gitlab_ci_runner::install::linux {
+class gitlab_ci_runner::install {
 
   case $::kernel {
     'Linux':  { include ::gitlab_ci_runner::install::linux }
     'windows': { include ::gitlab_ci_runner::install::windows }
     default: { error("Operatingsystem $::{'operatingsystem'} is not supported.") }
   }
-
 }
